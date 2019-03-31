24bc0a69280a01ab592d436d7b0c3f7daa8762c0
Jack Morrison <901jackmorrison@gmail.com>
2019-03-31 11:06:49 -0400

    Adds solution for problem 1.5

---
+++ CTCI/chapter_1/1.5/one_away.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+def main():
+
+    welcomeString = """
+Give me two strings, and I'll tell you if they're one edit away!
+
+There are 3 types of edits.
+1) Replacement (ex. `pale` --> `bale`)
+2) Insertion   (ex. `pale` --> `pales`)
+3) Deletion    (ex. `pale` --> `pae`)
+"""
+
+    print(welcomeString)
+
+    stringA = input("Input stringA: ")
+    stringB = input("Input stringB: ")
+
+    # Insertion and Deletion will be considered together, as they're identical opposite operations.
+    # Replacement will be considered separately, and only in the case where stringA and stringB have the same length.
+
+    if (len(stringA) == len(stringB)):
+        oneEditAway = evalReplacement(stringA, stringB)
+    else:
+        oneEditAway = evalInsertOrDelete(stringA, stringB)
+
+    print("'{}' and '{}' are one edit away?: {}".format(stringA, stringB, oneEditAway))
+
+    return 1
+
+
+def evalReplacement(A, B):
+    mismatch = 0
+
+    for i in range(0, len(A)):
+        if A[i] != B[i]:
+            mismatch += 1
+        if mismatch > 1:
+            return False
+
+    return True
+
+
+def evalInsertOrDelete(A, B):
+    A = set(A)
+    B = set(B)
+    
+    if len(A) > len(B):
+        diff = A.difference(B)
+    else:
+        diff = B.difference(A)
+
+    if len(diff) == 1:
+        return True
+    else:
+        return False
+
+main()
